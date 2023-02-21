960a6f2b0c588842c2ab75a050467ca79c814f2d
Silvio F. Coutinho <silviocoutinho@ymail.com>
2023-02-21 08:22:09 -0300

    fix: add return data from function

--- app/app.php
+++ app/app.php
@@ -1,11 +1,13 @@
 <?php
     define('API_BASE', 'http://api-php5.dvl.to/api/index.php?option=');
 
+    echo '<h3>APLICAÇÃO</h3><hr>';
+
     $resultado = api_request('status');
     echo $resultado;
 
     function api_request($option){
         $client = curl_init(API_BASE . $option);
         curl_setopt($client, CURLOPT_RETURNTRANSFER, TRUE);
-        $response = curl_exec($client);
+        return $response = curl_exec($client);
     }

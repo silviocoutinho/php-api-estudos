5944a3d6cd363808e01ca4b4a83f75956167bc43
Silvio Coutinho <silviocoutinho@ymail.com>
2023-02-22 14:24:59 -0300

    fix: curl and curl_exec adapt to php 5.3

--- app/app.php
+++ app/app.php
@@ -1,6 +1,6 @@
 <?php
     header('Content-Type: text/html; charset=utf-8');
-    define('API_BASE', 'http://api-php5.dvl.to/api/index.php?option=');
+    define('API_BASE', 'http://php5-api.dvl.to/api/index.php?option=');
 
     echo '<h3>APLICAÇÃO</h3><hr>';
 
@@ -8,10 +8,17 @@
 
     echo '<pre>';
     print_r($resultado);
+    echo '</pre>';
 
     function api_request($option){
-        $client = curl_init(API_BASE . $option);
-        curl_setopt($client, CURLOPT_RETURNTRANSFER, TRUE);
-        $response = curl_exec($client);
+        echo '<h3>'.API_BASE.$option.'</h3>';
+        $client = API_BASE . $option;
+
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $client);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+
+        $response = curl_exec($curl);
+
         return json_decode($response, true);
     }
